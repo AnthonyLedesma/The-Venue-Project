7bd238fa96d8e8a8a81587eb9e6f7c6b9b506956
paranoia1906 <anthonymledesma@gmail.com>
2018-08-01 10:52:14 -0700

    Pricing section and animations are added to project. Location area comes next.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from './components/header_footer/Header';
 import Featured from './components/featured/index';
 import VenueNfo from './components/venuNfo';
 import Highlight from './components/highlights';
+import Pricing from './components/pricing';
 
 class App extends Component {
   render() {
@@ -14,6 +15,7 @@ class App extends Component {
         <Featured/>
         <VenueNfo/>
         <Highlight/>
+        <Pricing/>
       </div>
     );
   }
